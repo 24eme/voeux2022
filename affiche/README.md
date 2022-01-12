f32ba52bae3c424361e1315e5a369009e3f47f22
Tangui Morlier <tangui@tangui.eu.org>
2022-01-12 10:51:48 +0100

    Optionnalisation du qrcode (/!\ l'ordre des arguments change)

--- affiche/README.md
+++ affiche/README.md
@@ -1,6 +1,6 @@
 # Generation affiche
 
-    bash bin/generate.sh template/dessin.svg "2022 avec" "Foo Bar" "Parce que je le mérite vraiment" fond/template_fond.png footer/oblique_24eme.png tenue/template_tenue.png qrcode/template_qrcode.png output/output.pdf
+    bash bin/generate.sh output/output.pdf template/dessin.svg "2022 avec" "Foo Bar" "Parce que je le mérite vraiment" fond/template_fond.png footer/oblique_24eme.png tenue/template_tenue.png qrcode/template_qrcode.png
 
 genère :
 
--- affiche/bin/generate.sh
+++ affiche/bin/generate.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-template=$1 #in template/ 
-aaa=$2
-bbb=$3
-ccc=$4
-fond=$5
-footer=$6
-tenue=$7
-qrcode=$8
-output=$9 #in output/
+output=$1 #in output/
+template=$2 #in template/
+aaa=$3
+bbb=$4
+ccc=$5
+fond=$6
+footer=$7
+tenue=$8
+qrcode=$9
 
 echo "s|fond/template_fond.png|"$fond"|" > $output".sed"
-echo "s|qrcode/template_qrcode.png|"$qrcode"|" >> $output".sed"
 echo "s|tenue/template_tenue.png|"$tenue"|" >> $output".sed"
 echo "s|footer/template_footer.png|"$footer"|" >> $output".sed"
 echo "s|AAATEMPLATEAAA|"$aaa"|" >> $output".sed"
 echo "s|BBBTEMPLATEBBB|"$bbb"|" >> $output".sed"
 echo "s|CCCTEMPLATECCC|"$ccc"|" >> $output".sed"
-
-cat $template | sed -f $output".sed" > $output".svg"
+if test "$qrcode"; then
+echo "s|qrcode/template_qrcode.png|"$qrcode"|" >> $output".sed"
+else
+echo "s|inkscape:label=\"intérieur tête\" * style=\"display:inline\"|inkscape:label=\"intérieur tête\" style=\"display:none\"|" >> $output".sed"
+fi
+cat $template | tr '\n' ' ' | sed -f $output".sed" > $output".svg"
 
 inkscape $output".svg" --export-area-page --batch-process --export-type=pdf --export-filename=$output
 inkscape $output".svg" --export-area-page --batch-process --export-type=png --export-filename=$output".png"
