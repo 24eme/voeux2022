c33166f93166d7a886f554f30612a922ac451dea
Tangui Morlier <tangui@tangui.eu.org>
2022-01-12 10:39:46 +0100

    Optionnalisation du footer

--- affiche/bin/generate.sh
+++ affiche/bin/generate.sh
@@ -5,13 +5,15 @@ aaa=$2
 bbb=$3
 ccc=$4
 fond=$5
-tenue=$6
-qrcode=$7
-output=$8 #in output/
+footer=$6
+tenue=$7
+qrcode=$8
+output=$9 #in output/
 
 echo "s|fond/template_fond.png|"$fond"|" > $output".sed"
 echo "s|qrcode/template_qrcode.png|"$qrcode"|" >> $output".sed"
 echo "s|tenue/template_tenue.png|"$tenue"|" >> $output".sed"
+echo "s|footer/template_footer.png|"$footer"|" >> $output".sed"
 echo "s|AAATEMPLATEAAA|"$aaa"|" >> $output".sed"
 echo "s|BBBTEMPLATEBBB|"$bbb"|" >> $output".sed"
 echo "s|CCCTEMPLATECCC|"$ccc"|" >> $output".sed"
