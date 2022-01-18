1b7fca879cd0b959f3ba736a29b11df6fcb49231
Vincent LAURENT <vince.laurent@gmail.com>
2022-01-18 12:57:34 +0100

    Redimensionnement des miniature de footer

--- affiche/bin/miniature.sh
+++ affiche/bin/miniature.sh
@@ -2,9 +2,5 @@ rm -f fond/miniature/*
 mogrify -resize 300x -shave 0x70 -gravity Center -path fond/miniature/ -format jpg fond/*.png
 rm -f tenue/miniature/*
 mogrify -resize x300 -shave 50x0 -gravity center -path tenue/miniature/ -format png tenue/*.png
-cd template
-rm -f miniature/*
-mogrify -resize 300x -path miniature/ -format png *.svg
-cd -
 rm -f footer/miniature/*
-mogrify -resize x300 -crop 50x -gravity South -path footer/miniature/ -format png footer/*.png
+mogrify -resize x500 -chop x400 -gravity North -path footer/miniature/ -format png footer/*.png
