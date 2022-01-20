5cb17597e294a2b9b7e2ea93f02a343f0ed5678a
Vincent LAURENT <vince.laurent@gmail.com>
2022-01-20 12:17:38 +0100

    Utilisation du query string

--- public/q.php
+++ public/q.php
@@ -2,16 +2,16 @@
 
 require __DIR__.'/config.inc.php'; 
 
-if(!isset($_GET['q']) || !preg_match("/^[a-z0-9]+$/", $_GET['q'])) {
-    echo "Paramètre \"q\" non renseigné ou invalide";
+if(!isset($_SERVER['QUERY_STRING']) || !preg_match("/^[a-z0-9]+$/", $_SERVER['QUERY_STRING'])) {
+    echo "Code non renseigné ou invalide";
     exit(1);
 }
 
-if(!file_exists(DB_DIR."/".$_GET['q'].".csv")) {
+if(!file_exists(DB_DIR."/".$_SERVER['QUERY_STRING'].".csv")) {
     echo "Ce code n'existe pas";
     exit(1);
 }
 
-$csv = file_get_contents(DB_DIR."/".$_GET['q'].".csv");
+$csv = file_get_contents(DB_DIR."/".$_SERVER['QUERY_STRING'].".csv");
 
 header('Location: camera.php?csv='.urlencode($csv));
