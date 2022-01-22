25dd95a8e306f8878d005cc7f336d8963807fafe
Tangui Morlier <tangui@tangui.eu.org>
2022-01-22 22:18:32 +0100

    Avec des erreurs

--- public/photo.php
+++ public/photo.php
@@ -2,13 +2,21 @@
 
 require __DIR__.'/config.inc.php'; 
 
+if (!$_FILES["camera"] || (isset($_FILES["camera"]['error']) && $_FILES["camera"]['error'])) {
+    echo "<p>Erreur lors de l'upload.</p>";
+    echo "<pre>";
+    print_r($_FILES);
+    echo "</pre>";
+    exit(2);
+}
+
 if($_FILES["camera"]['type'] != 'image/png' || mime_content_type($_FILES["camera"]['tmp_name']) != 'image/png') {
-    echo "Ce format de fichier n'est pas autorisé";
+    echo "<p>Ce format (".$_FILES["camera"]['type'].") de fichier n'est pas autorisé.</p>";
     exit(1);
 }
 
 if(!is_writable(UPLOAD_DIR)) {
-    echo "Le dossier d'upload n'est pas autorisé en écriture";
+    echo "<p>Le dossier d'upload (".UPLOAD_DIR.") n'est pas autorisé en écriture.</p>";
     exit(1);
 }
 
