e8fabbf2abf0ae1dc7e5a1a272442f2fb069c699
Tangui Morlier <tangui@tangui.eu.org>
2022-01-27 16:53:03 +0100

    Réuni plusieurs PDF en un

--- affiche/bin/generate_from_csv.sh
+++ affiche/bin/generate_from_csv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cat $1 | grep ';' | awk '{gsub("'"'"'", "'"'"'\"'"'"'\"'"'"'", $0); print "wget --content-disposition '"'"'https://voeux.24eme.fr/2022/affiche.php?format=pdf&csv="$0"'"'"';"}' | bash
+ls 2*.pdf | sed s'/.._.*//'| uniq | while read id ; do pdftk "$id"* cat output "merged_"$id".pdf";  done
