936a3a5c60f6604e3514080a980fbd08f3db16c8
Tangui Morlier <tangui@tangui.eu.org>
2022-01-31 17:23:35 +0100

    Gestion du lien public qui fonctionne dans le qrcode

--- public/q.php
+++ public/q.php
@@ -2,26 +2,27 @@
 
 require __DIR__.'/config.inc.php'; 
 
-if(!isset($_SERVER['QUERY_STRING']) || !preg_match("/^[a-z0-9_]+$/", $_SERVER['QUERY_STRING'])) {
+if(!isset($_SERVER['QUERY_STRING']) || !preg_match("/^[a-z0-9_]+/", $_SERVER['QUERY_STRING'])) {
     echo "Code non renseigné ou invalide";
     exit(1);
 }
 
 $parameters = explode('_', $_SERVER['QUERY_STRING']);
 $csvId = $parameters[0];
+$csvId = str_replace('&public=true', '', $csvId);
 $numero = null;
 if($parameters[1]) {
     $numero = $parameters[1];
 }
 
 if(!file_exists(DB_DIR."/".$csvId.".csv")) {
-    echo "Ce code n'existe pas";
+    echo "Ce code ($csvId) n'existe pas";
     exit(1);
 }
 
 $csv = file_get_contents(DB_DIR."/".$csvId.".csv");
 
-if($numero !== null) {
+if(!(@$_GET['public'])  && ($numero !== null)) {
     header('Location: resultat.php?csv='.urlencode($csv)."&numero=".$numero);
     exit;
 }
